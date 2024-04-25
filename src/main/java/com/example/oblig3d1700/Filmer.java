068c5accbb5fb2bf139a2b7437e7ee38a6379240
akaran0310 <s376605@oslomet.no>
2024-04-25 12:48:12 +0200

    Initial commit

---
+++ src/main/java/com/example/oblig3d1700/Filmer.java
@@ -0,0 +1,21 @@
+package com.example.oblig3d1700;
+
+
+    public class Filmer {
+        private String film;
+
+        public Filmer( String film ){
+
+            this.film=film;
+        }
+
+
+        public String getFilm() {
+            return film;
+        }
+
+        public void setFilm(String film) {
+            this.film = film;
+        }
+    }
+
